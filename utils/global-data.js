d0591b8a6382df9b30546baeb066e053b51b9eac
unclemantis.btc <unclemantis@gmail.com>
2022-05-19 02:17:15 -0400

    U title and theme

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,11 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'unclemantis.btc';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Stacks Blockcbain, DeFi, and the beginning
+      of the 4th industrial revolution.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
